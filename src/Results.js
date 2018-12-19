48fb307b2e745fa9f24b3136e9188876865586cf
Juliano Rafael <juliano@getty.io>
2018-12-18 22:59:26 -0200

    :sparkles: Component that shows all the highlights for a certain color

---
+++ src/Results.js
@@ -0,0 +1,16 @@
+import React from "react"
+import "./Results.css"
+
+const Results = ({ highlights, color, text }) => (
+  <ul className="results">
+    {highlights.filter(hg => hg.color === color).map(hg => (
+      <li key={hg.id}>
+        <span className={hg.color}>
+          {text.slice(hg.range.start, hg.range.end)}
+        </span>
+      </li>
+    ))}
+  </ul>
+)
+
+export default Results
